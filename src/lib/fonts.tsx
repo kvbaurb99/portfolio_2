769186bcd04722060e46d026f35e0da4f8b5a7d1
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-05-12 22:51:16 +0200

    configure libraries

---
+++ src/lib/fonts.tsx
@@ -0,0 +1,10 @@
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  variable: "--font-roboto-sans",
+  subsets: ["latin"],
+});
+
+const fonts = { roboto };
+
+export default fonts;
