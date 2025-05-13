8c8906fe91a20cdc0c8a966bdb558790aa51718e
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-05-13 09:37:35 +0200

    remove dynamic imports from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,7 @@ import ExperienceSection from "@/components/experience/Experience";
 import SoftSkills from "@/components/skills/soft/SoftSkills";
 import TechnicalSkills from "@/components/skills/technical/TechnicalSkills";
 import Header from "@/components/ui/header/Header";
-import { Loader2 } from "lucide-react";
-import dynamic from "next/dynamic";
-
-const PortfolioSection = dynamic(
-  () => import("@/components/portfolio/Portfolio"),
-  {
-    loading: () => <Loader2 className="animate-spin" size={16} />,
-    ssr: true,
-  }
-);
+import PortfolioSection from "@/components/portfolio/Portfolio";
 
 export default function Home() {
   return (
